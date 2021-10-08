c8b62a994d46d703e9fc1cfa6fa1b1520e7eabf5
Caio Cruz <caio.vcruz@outlook.com>
2021-10-07 21:22:53 -0300

    Layout Responsivo + Forms

--- Front/ProEventos-App/src/app/shared/nav/nav.component.ts
+++ Front/ProEventos-App/src/app/shared/nav/nav.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
   isCollapsed = true;
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit(): void {}
+
+  public showMenu(): boolean {
+    return this.router.url !== '/user/login';
   }
-
 }
